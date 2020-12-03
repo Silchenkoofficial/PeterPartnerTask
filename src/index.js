9bf9459ba4702db647b9595b5b01f0af42250bcd
Nikita <nikita.silchenko.work@yandex.ru>
2020-12-04 02:00:10 +0300

    on macOS

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// import {} from 'react-router-dom';
 import App from './App';
+
+import 'styles/css/Main.min.css';
 
 ReactDOM.render(
     <App />,
